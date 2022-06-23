804991b84c3c36bd570b729d11118a8f0c453d8c
Dang Dinh Nhan <dinhnhandtu@gmail.com>
2022-06-23 17:18:52 +0700

    rebuild

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Github -Travis CI - AWS EB</p>
+        <p>Github -Travis CI</p>
         <a
           className="App-link"
           href="https://reactjs.org"
