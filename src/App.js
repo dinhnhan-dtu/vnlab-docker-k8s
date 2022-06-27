bfee9bec760c9f74fdb03e5fec5a819ac8180f51
Dang Dinh Nhan <dinhnhandtu@gmail.com>
2022-06-27 17:09:28 +0700

    test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Github -Travis CI</p>
+        <p>Github -Travis CI New Version</p>
         <a
           className="App-link"
           href="https://reactjs.org"
